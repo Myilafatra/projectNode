537f03e91c5ff2cd0dc8518d33c1a7bd8f847ad1
rijaprincy <miaryravaoarisoa2@gmail.com>
2019-04-15 15:21:53 +0300

    modifier

--- Project/Back/Route/route.js
+++ Project/Back/Route/route.js
@@ -1,34 +1,129 @@
-    var url = require('url');
-    var fs = require('fs');
-  
-    module.exports = {
-        handleRequest(req,res){
-            
-            var page = url.parse(req.url).pathname;
-            var lister = fs.readFileSync('./note.json', 'utf8');
-            var tester = fs.readFileSync('./test.json', 'utf8');
-   
-        if (page == '/') {
-            res.write('');
-        }
-        else if (page == '/list') {
-            res.write(lister);
-        }
-        else if (page == '/test') {
-            res.write(tester);
-        }
-        res.end();
-        }
-    }
-    
-//     var http = require('http'); 
-// var fs = require('fs'); 
+const express = require('express');
+const fs = require('fs');
 
-// var server = http.createServer(function (req, res) { 
-//     fs.readFile(__dirname + '/data.txt', function (err, data) { 
-//         res.end(data); 
-//     }); 
-// }); 
-// server.listen(8000);
+module.exports = function (app){
+    app.post('/', function (req, res) {
+        res.send('')
+   })
+   app.post('/list', function (req, res) {
+        try {
+             var read = fs.readFileSync('./Model/note.json', 'utf8')
+             var parser = JSON.parse(read)
+             
+             for (let i = 0; i < parser.length; i++) {
+                 
+                  app.post(parser[i].Image, function (req, res) {
+                       try {
+                            var iamageRouter = '.' + parser[i].Image
+                            var lireImageRouter = fs.readFileSync(iamageRouter)
+                            console.log(lireImageRouter)
+                            res.write(lireImageRouter)
+                            res.end()
+   
+                       } catch (e) {
+                            console.log(e.stack);
+                       }
+                  })
+             }
+             res.write(read)
+             res.end()
+        } catch (ev) {
+             console.log(ev.stack)
+        }
+   })
+   
+   app.get('/test', function (req, res) {
+        let read = fs.readFileSync('./Model/test.json', 'utf8')
+        res.write(read)
+        res.end()
+   })
+   app.post('/test',function(req, res){
+       
+        
+        var Nom = req.body.Nom
+        var Prenom = req.body.Prenom
+        try {
+             var r = fs.readFileSync('./Model/test.json', 'utf8')
+             var s = JSON.parse(r)
+/*              var count = Math.max(s.length)
+             var Id = count +1
+ */
+             var Id = s[s.length-1].Id +1;
+             s.push({"Id":Id,"Nom":Nom,"Prenom":Prenom})
+        
+        } catch(e){
+             console.log(e.stack);   
+        }
+        fs.writeFileSync('./Model/test.json',JSON.stringify(s))
+   
+        res.send(s)
+        //console.log(s);
+        
+   
+   })
+   
+   app.put('/test',function(req, res){
+        var Id = req.body.Id
+        var Nom = req.body.Nom
+        var Prenom = req.body.Prenom
+        var r = fs.readFileSync('./Model/test.json', 'utf8')
+        var s = JSON.parse(r)
+        for(let i=0;i<s.length;i++){
+        if(s[i].Id== Id){
+   
+             if(typeof Nom=="string"){
+                  s[i].Nom=Nom;
+             }
+             if(typeof Prenom=="string"){
+                  s[i].Prenom=Prenom;
+             }
+        }
+        }
+             console.log(s); 
+             res.send(s)
+             fs.writeFileSync('./Model/test.json',JSON.stringify(s))
+        
+   
+   })
+   
+   
+   app.delete('/test',function(req, res){
+             var Id=req.body.Id
+             var r=fs.readFileSync('./Model/test.json','utf-8')
+             var s= JSON.parse(r)
+             for(var i=0;i<s.length;i++){
+                  if(s[i].Id==Id){
+                       s.splice(i,1)
+                  }    
+             }
+                   res.send(s) 
+                   fs.writeFileSync('./Model/test.json', JSON.stringify(s))
+                  
+                   console.log(s); 
+           });
+       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+     
 
 
--- Project/Back/serveur.js
+++ Project/Back/serveur.js
@@ -1,8 +1,17 @@
-var http = require('http');
-var router = require('./Route/route.js');
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const router = express.Router()
+const bodyParser = require('body-parser');
 
-http.createServer(router.handleRequest).listen(8080);
-console.log(`Server demarer `);
+const rout = require('./Route/route')
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+rout(app)
 
-
+app.listen(8080, function () {
+  console.log('server demarer')
+})
+app.use('/', router)
